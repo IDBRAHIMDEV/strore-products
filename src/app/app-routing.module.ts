f2a34510c97e2d7596d4139cdeeb4c2ab8fd4d27
Taha EL MALIANI <taha.elmaliani.justice@outlook.fr>
2019-05-04 11:50:57 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AddProductComponent } from './components/add-product/add-product.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { ShowProductComponent } from './components/show-product/show-product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : '', redirectTo: '/products', pathMatch: 'full'},
+  { path:'products', component: ListProductComponent },
+  { path:'products/add', component: AddProductComponent },
+  { path:'products/edit/:id', component: EditProductComponent },
+  { path:'products/:id', component: ShowProductComponent },
+  { path:'register', component: RegisterComponent },
+  { path:'login', component: LoginComponent },
+  { path:'**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
+import { ShowProductComponent } from './components/show-product/show-product.component';
+import { EditProductComponent } from './components/edit-product/edit-product.component';
+import { ListProductComponent } from './components/list-product/list-product.component';
+import { AddProductComponent } from './components/add-product/add-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    RegisterComponent,
+    LoginComponent,
+    PageNotFoundComponent,
+    ShowProductComponent,
+    EditProductComponent,
+    ListProductComponent,
+    AddProductComponent
   ],
   imports: [
     BrowserModule,
